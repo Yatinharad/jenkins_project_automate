a861a2dd65d215a6775c16a70f9cca59ef47ab5f
Ignatius <ignatius128@gmail.com>
2024-08-26 15:45:02 +0530

    first commit

---
+++ application/views/user/footer.php
@@ -0,0 +1,35 @@
+<footer>
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-12">
+              <p>Copyright &copy; 2024 League of Fitness
+<!--               
+              - Designed by <a rel="nofollow" href="https://templatemo.com" class="tm-text-link" target="_parent">TemplateMo</a></p>
+               -->
+              <!-- You shall support us a little via PayPal to info@templatemo.com -->
+              
+          </div>
+      </div>
+  </div>
+</footer>
+
+<!-- jQuery -->
+<script src="assets/js/jquery-2.1.0.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<!-- Plugins -->
+<script src="assets/js/scrollreveal.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/imgfix.min.js"></script> 
+<script src="assets/js/mixitup.js"></script> 
+<script src="assets/js/accordions.js"></script>
+
+<!-- Global Init -->
+<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
