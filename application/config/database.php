0a2d6c13ea39a70604267fb7faacf96b77c434b4
unknown <yatinharad2002@gmail.com>
2024-09-01 13:11:57 +0530

    add jenkins file

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '172.17.0.2',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'lof',
